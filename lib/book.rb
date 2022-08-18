1c00c3c83cde7c24a5549d6238e6cd467a9c5cd6
davidnene <davienesh4@gmail.com>
2022-08-18 09:36:49 +0300

    commit tree 4a38f7e5ea966da819ddb7884308b72c9fed5bb8

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+require "pry"
+class Book
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+b1 = Book.new("River Between")
+b1.title
+
+b1.author = "Thion\'go"
+b1.author
+
+b1.page_count = 272
+b1.page_count
+
+b1.turn_page
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
 # Make your shoe class here!
+require "pry"
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+sh1 = Shoe.new("Nike")
+sh1.brand
+sh1.color = "Blue"
+sh1.size = 7
+sh1.material = "leather"
+sh1.condition = "old"
+sh1.cobble
+
+
